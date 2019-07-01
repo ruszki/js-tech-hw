14447ba17eb1ee7a4e4783a5d089cf51c2bf08e2
Gergo Vladiszavlyev <me@ruszki.com>
2019-07-01 20:34:31 +0200

    Add test for secret code input field

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(() => {
+  return {};
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('should contains input for secret code', () => {
+  shallow(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  ).contains('input#CodeInputId');
 });
